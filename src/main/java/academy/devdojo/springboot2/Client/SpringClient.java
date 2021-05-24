3e25dde2c88383436ffbb88778343820d184cf92
DaviCorreia <davicorreia21@gmail.com>
2021-05-23 22:32:21 -0300

    RestTemplate PUT and DELETE

--- src/main/java/academy/devdojo/springboot2/Client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/Client/SpringClient.java
@@ -3,8 +3,7 @@ package academy.devdojo.springboot2.Client;
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -31,6 +30,38 @@ public class SpringClient {
                     });
 
             log.info(exchange.getBody());
+
+            Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+            System.out.println(samuraiChamploo.getName());
+            ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                    HttpMethod.POST,
+                    new HttpEntity<>(samuraiChamploo,createJsonHearder()),
+                    Anime.class);
+            log.info("saved anime{}",samuraiChamplooSaved);
+
+            Anime animeToBeUpdate = samuraiChamplooSaved.getBody();
+            animeToBeUpdate.setName("Boruto");
+
+            ResponseEntity<Void> samuraiChamplooUpdated = new RestTemplate().exchange("http://localhost:8080/animes/",
+                    HttpMethod.PUT,
+                    new HttpEntity<>(animeToBeUpdate,createJsonHearder()),
+                    Void.class);
+
+            log.info(samuraiChamplooUpdated);
+
+            ResponseEntity<Void> samuraiChamplooDelete = new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+                    HttpMethod.DELETE,
+                    null,
+                    Void.class,
+                    animeToBeUpdate.getId());
+
+            log.info(samuraiChamplooDelete);
+        }
+
+        private static HttpHeaders createJsonHearder(){
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            return httpHeaders;
         }
     }
 
--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -4,6 +4,7 @@ package academy.devdojo.springboot2.requests;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePostRequestBody {
